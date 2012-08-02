a36dd6c123bebf26f392dbd8fc9a787ba9458e9e
jogwen <joanna.wakeford+github@gmail.com>
2012-08-02 18:27:08 +0100

    Completed stage 8.

--- 8.py
+++ 8.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
 url = "http://www.pythonchallenge.com/pc/def/integrity.html"
+title = "working hard?"
+auth_popup_message = "inflate"
+url2 = "http://www.pythonchallenge.com/pc/return/good.html"
 
 if __name__ == "__main__":
+    import bz2
     import re
 
     import utils
 
     pattern = re.compile(r"<!--\nun: '(.+)'\s+pw: '(.+)'")
     un, pw = re.findall(pattern, utils.wget(url))[0]
-    print un
-    print pw
+    #print un
+    #print pw
+
+    print "un: %s" % bz2.decompress(un.decode('string_escape'))
+    print "pw: %s" % bz2.decompress(pw.decode('string_escape'))
 
